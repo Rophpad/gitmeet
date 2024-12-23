8000af72538026366f0f43bdc525709ede88d4b2
rophpad <rophenp@gmail.com>
2024-12-23 20:57:11 +0100

    Add files

---
+++ src/app/components/ui/draft.tsx
@@ -0,0 +1,16 @@
+export default function DraftComponent() {
+    return <div>
+        <header>
+            <h1>gitme</h1>
+            <nav>
+                <ul>
+                    <li>Call History</li>
+                    <li>Developers</li>
+                    <li>Stats</li>
+                </ul>
+            </nav>
+            <h1>Connect</h1>
+        </header>
+    </div>;
+  }
+  
